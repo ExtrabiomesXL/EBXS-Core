68abe6703a9e841781ed155194c822e7a7c5b156
Ammon Lauritzen <allaryin@gmail.com>
2015-05-11 12:59:54 -0500

    Fleshed out generic decoration generator.

--- src/main/java/extrabiomes/lib/ExtraWorldGenerator.java
+++ src/main/java/extrabiomes/lib/ExtraWorldGenerator.java
@@ -1,8 +1,10 @@
 package extrabiomes.lib;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -11,12 +13,28 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 import cpw.mods.fml.common.IWorldGenerator;
 import extrabiomes.lib.settings.BiomeSettings;
 
-public abstract class ExtraWorldGenerator implements IWorldGenerator {
+public class ExtraWorldGenerator implements IWorldGenerator {
 
+	protected Set<ExtrabiomeGenBase> biomes = Sets.newHashSet();
 	protected Map<String,WorldGenerator> worldGens = Maps.newHashMap();
+	
+	public final int maxDecorations;
+	
+	public ExtraWorldGenerator(int max) {
+		this.maxDecorations = max;
+	}
 	
 	public void registerGenerator(String type, WorldGenerator gen) {
 		worldGens.put(type, gen);
+	}
+	
+	public void registerBiome(ExtrabiomeGenBase biome) {
+		biomes.add(biome);
+	}
+	public void registerBiome(BiomeSettings settings) {
+		if( settings.getBiome().isPresent() ) {
+			registerBiome( settings.getBiome().get() );
+		}
 	}
 	
 	protected int applyGenerator(String type, World world, int chunkX, int chunkZ, Random rand, int times) {
@@ -50,11 +68,30 @@ public abstract class ExtraWorldGenerator implements IWorldGenerator {
 		}
 		return false;
 	}
-
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		chunkX <<= 4;
+		chunkZ <<= 4;
+		final BiomeGenBase biome = world.getBiomeGenForCoords(chunkX, chunkZ);
+		
+		if( biomeCheck(biome) ) {
+			final ExtrabiomeGenBase eBiome = (ExtrabiomeGenBase)biome;
+			for( int k = 0; k < maxDecorations; ++k ) {
+				final int idx = random.nextInt( worldGens.size() );
+				applyGenerator( (String)worldGens.keySet().toArray()[idx], world, chunkX, chunkZ, random );
+			}
+		}
+	}
 
 	protected boolean biomeCheck(BiomeSettings settings, BiomeGenBase biome) {
 		return settings.getBiome().isPresent()
 				&& biome == settings.getBiome().get();
+	}
+	
+	protected boolean biomeCheck(BiomeGenBase biome) {
+		return biomes.contains(biome);
 	}
 
 }
