adfa7d87e9b63979f25a9a2c0fa7a9f4db409bd6
Ammon Lauritzen <allaryin@gmail.com>
2015-05-29 12:02:47 -0500

    Added new explicit biome height definition for use by Meadows.

--- src/main/java/extrabiomes/lib/worldgen/ExtrabiomeGenBase.java
+++ src/main/java/extrabiomes/lib/worldgen/ExtrabiomeGenBase.java
@@ -6,12 +6,32 @@ import java.util.Random;
 import com.google.common.collect.Lists;
 
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.BiomeGenBase.Height;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.common.BiomeDictionary.Type;
 import extrabiomes.lib.settings.BiomeSettings;
 
 public abstract class ExtrabiomeGenBase extends BiomeGenBase {
+	/*
+	 * Vanilla provides the following height definitions, which we should use where appropriate
+	 * 
+					    height_Default				= new Height(0.1F, 0.2F);
+					    height_ShallowWaters		= new Height(-0.5F, 0.0F);
+					    height_Oceans				= new Height(-1.0F, 0.1F);
+					    height_DeepOceans			= new Height(-1.8F, 0.1F);
+					    height_LowPlains			= new Height(0.125F, 0.05F);
+					    height_MidPlains			= new Height(0.2F, 0.2F);
+					    height_LowHills				= new Height(0.45F, 0.3F);
+					    height_HighPlateaus			= new Height(1.5F, 0.025F);
+					    height_MidHills				= new Height(1.0F, 0.5F);
+					    height_Shores				= new Height(0.0F, 0.025F);
+					    height_RockyWaters			= new Height(0.1F, 0.8F);
+					    height_LowIslands			= new Height(0.2F, 0.3F);
+					    height_PartiallySubmerged	= new Height(-0.2F, 0.1F);
+    */
+	protected final static Height height_FlatPlains = new Height(0.1F, 0.03125F);	// between Shores and LowPlains
+	
 	protected BiomeSettings settings;
 	protected Type[] typeFlags;
 	
