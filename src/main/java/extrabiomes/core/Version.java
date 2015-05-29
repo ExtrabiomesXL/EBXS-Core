2508399a4d07a9972e7bd80dbda8ad6809ce53b3
Ammon Lauritzen <allaryin@gmail.com>
2015-05-29 12:17:53 -0500

    Imported dye from ebxl.

--- src/main/java/extrabiomes/core/Version.java
+++ src/main/java/extrabiomes/core/Version.java
@@ -1,11 +1,13 @@
 package extrabiomes.core;
 
+import java.util.Locale;
+
 import extrabiomes.lib.Const;
 
 public abstract class Version {
-	public static final String	MOD_ID		= Const.API_MOD_ID;
-	public static final String	MOD_NAME	= "Extrabiomes Core";
-	public static final String	VERSION		= Const.API_VERSION;
+	public static final String	MOD_ID			= Const.API_MOD_ID;
+	public static final String	MOD_NAME		= "Extrabiomes Core";
+	public static final String	VERSION			= Const.API_VERSION;
 
-	public static final String	CHANNEL		= MOD_ID;	
+	public static final String	CHANNEL			= MOD_ID;
 }
--- src/main/java/extrabiomes/lib/Const.java
+++ src/main/java/extrabiomes/lib/Const.java
@@ -1,13 +1,17 @@
 package extrabiomes.lib;
 
+import java.util.Locale;
+
 public abstract class Const {
-	public static final String PREFIX		= "ebxs";
-	public static final String PREFIX_LONG	= "extrabiomes";
+	public static final String PREFIX			= "ebxs";
+	public static final String PREFIX_LONG		= "extrabiomes";
 	
-	public static final String API_MOD_ID	= "ExtrabiomesCore";
-	public static final String API_VERSION	= "0.1.2";
+	public static final String API_MOD_ID		= "ExtrabiomesCore";
+	public static final String API_VERSION		= "0.1.2";
 	
-	public static final String CATEGORY_BIOME = "biome";
-	public static final String CATEGORY_BLOCK = "block";
-	public static final String CATEGORY_ITEM = "item";
+	public static final String CATEGORY_BIOME	= "biome";
+	public static final String CATEGORY_BLOCK	= "block";
+	public static final String CATEGORY_ITEM	= "item";
+	
+	public static final String TEXTURE_PATH		= PREFIX_LONG;
 }
