655c778eb5fa331d592cbe85e63210f0665b776a
Ammon Lauritzen <allaryin@gmail.com>
2015-06-10 10:07:46 -0500

    Update to support version number in props file.

--- src/main/java/extrabiomes/core/Version.java
+++ src/main/java/extrabiomes/core/Version.java
@@ -7,7 +7,8 @@ import extrabiomes.lib.Const;
 public abstract class Version {
 	public static final String	MOD_ID			= Const.API_MOD_ID;
 	public static final String	MOD_NAME		= "Extrabiomes Core";
-	public static final String	VERSION			= Const.API_VERSION;
+	public static final String	VERSION			= "@VERSION@";
+	public static final String	API_VERSION		= Const.API_VERSION;
 
 	public static final String	CHANNEL			= MOD_ID;
 }
