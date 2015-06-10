c3d01544d5b2f05cd4b2e184850b301f399412ff
Ammon Lauritzen <allaryin@gmail.com>
2015-06-10 11:38:08 -0500

    Bumping version to 0.1.3

--- src/main/java/extrabiomes/lib/Const.java
+++ src/main/java/extrabiomes/lib/Const.java
@@ -7,7 +7,7 @@ public abstract class Const {
 	public static final String PREFIX_LONG		= "extrabiomes";
 	
 	public static final String API_MOD_ID		= "ExtrabiomesCore";
-	public static final String API_VERSION		= "0.1.2";
+	public static final String API_VERSION		= "0.1.3";
 	
 	public static final String CATEGORY_BIOME	= "biome";
 	public static final String CATEGORY_BLOCK	= "block";
